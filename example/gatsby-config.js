7e52a745cb553c965960feecaf7b2f3eb1513a86
KtanPatel <34062262+KtanPatel@users.noreply.github.com>
2025-11-04 23:10:14 +0530

    fix(example): transpile problematic modules and re-enable example build in CI

--- example/gatsby-config.js
+++ example/gatsby-config.js
@@ -5,6 +5,19 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-sitemap`,
+    // Transpile certain node_modules that ship ESM TypeScript (.mts/.cts) or
+    // untranspiled code so Gatsby's query extractor can process them.
+    {
+      resolve: `gatsby-plugin-compile-es6-packages`,
+      options: {
+        modules: [
+          `@graphql-codegen`,
+          `graphql-http`,
+          `@graphql-tools`,
+          `@graphql-codegen/*`
+        ]
+      }
+    },
     {
       resolve: `gatsby-plugin-sitemap-html`,
       options: {
