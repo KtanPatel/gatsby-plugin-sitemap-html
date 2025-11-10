8db4bdf4c6f365e2ea15adcde8c27af2f2e67ccb
KtanPatel <34062262+KtanPatel@users.noreply.github.com>
2025-11-10 19:30:05 +0530

    chore: update example dependencies and remove unnecessary plugin

--- example/gatsby-config.js
+++ example/gatsby-config.js
@@ -15,21 +15,6 @@ module.exports = {
   plugins: [
     `gatsby-plugin-sitemap`,
     {
-      // Transpile problematic packages that ship .mts/.cts or untranspiled TS sources
-      // so Gatsby's query extractor won't try to parse them directly.
-      resolve: `gatsby-plugin-compile-es6-packages`,
-      options: {
-        modules: [
-          `@graphql-codegen`,
-          `@graphql-codegen/*`,
-          `graphql-http`,
-          `@graphql-tools`,
-          `@graphql-tools/*`,
-          `rxjs`,
-        ],
-      },
-    },
-    {
       resolve: `gatsby-plugin-sitemap-html`,
       options: {
         // optional: xslTemplate: `${__dirname}/src/templates/sitemap.xsl`
